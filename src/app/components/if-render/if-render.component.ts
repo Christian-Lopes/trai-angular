22e36fb7ff8698029d96f9e38a2a052dc6a6711d
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-05-20 16:38:11 -0300

    Renderização Condisional

---
+++ src/app/components/if-render/if-render.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-if-render',
+  templateUrl: './if-render.component.html',
+  styleUrls: ['./if-render.component.css']
+})
+export class IfRenderComponent implements OnInit {
+
+  canShow: boolean = true;
+
+  //name: string = "Christian";
+  name: string = "Fernando";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
