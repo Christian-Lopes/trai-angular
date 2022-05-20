ef403cb992f60ef0c3c3a4d09386e1d8e9125144
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-05-20 15:33:46 -0300

    Compartilhamento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "Railana Amoras";
+
+  userData = {
+    email: 'fulano@gmail.com',
+    role: 'Admin'
+};
+
   title = 'trai-angular';
 }
